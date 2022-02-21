575737af44e1a3a500c64194b3049ceb2c409585
Andrew Thigpen <andyt05@gmail.com>
2022-02-21 11:37:02 -0500

    Update debug.

--- cmd/debug.go
+++ cmd/debug.go
@@ -12,10 +12,25 @@ var debugCmd = &cobra.Command{
 	Use:   "debug",
 	Short: "Lists windows",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("Slack:")
+		pids, err := robotgo.FindIds("slack")
+		for _, pid := range pids {
+			fmt.Printf("%d %s\n", pid, robotgo.GetTitle(pid))
+		}
+		fmt.Println("")
+
+		fmt.Println("Teams:")
+		pids, err = robotgo.FindIds("teams")
+		for _, pid := range pids {
+			fmt.Printf("%d %s\n", pid, robotgo.GetTitle(pid))
+		}
+		fmt.Println("")
+
 		names, err := robotgo.FindNames()
 		if err != nil {
 			return err
 		}
+		fmt.Println("All names:")
 		for _, name := range names {
 			fmt.Println(name)
 		}
